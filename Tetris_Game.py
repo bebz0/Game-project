01a3a525721f967c633a01c921209827d2e18b4a
bebz0 <romanbizonua@gmail.com>
2025-05-28 20:39:29 +0300

    add _place_piece and reset

--- Tetris_Game.py
+++ Tetris_Game.py
@@ -40,3 +40,66 @@ class TetrisGame(GameBase):
         self.fall_timer = 0
         self.game_over = False
         self.game_state = "start"
+
+
+
+    def handle_input(self, event):
+        if event.type == pygame.QUIT:
+            return False
+        elif event.type == pygame.KEYDOWN:
+            if self.game_state == "start":
+                if event.key == pygame.K_RETURN:
+                    self.game_state = "playing"
+                elif event.key == pygame.K_ESCAPE:
+                    return False
+            elif self.game_state == "playing":
+                if self.game_over:
+                    if event.key == pygame.K_r:
+                        self.reset()
+                    elif event.key == pygame.K_ESCAPE:
+                        self.game_state = "start"
+                        self.reset()
+                else:
+                    new_x, new_y = self.current_piece.x, self.current_piece.y
+                    new_shape = self.current_piece.shape
+                    if event.key == pygame.K_LEFT:
+                        new_x -= 1
+                    elif event.key == pygame.K_RIGHT:
+                        new_x += 1
+                    elif event.key == pygame.K_DOWN:
+                        new_y += 1
+                    elif event.key == pygame.K_UP:
+                        new_shape = self.current_piece.rotate()
+                    elif event.key == pygame.K_SPACE:
+                        while self.grid.is_valid_position(self.current_piece, y=self.current_piece.y + 1):
+                            self.current_piece.y += 1
+                        self._place_piece()
+                        return True
+                    elif event.key == pygame.K_ESCAPE:
+                        self.game_state = "start"
+                        self.reset()
+                    if self.grid.is_valid_position(self.current_piece, new_shape, new_x, new_y):
+                        self.current_piece.x, self.current_piece.y = new_x, new_y
+                        self.current_piece.shape = new_shape
+        return True
+
+    def _place_piece(self):
+        cleared = self.grid.place_piece(self.current_piece)
+        self.score_manager.add_lines(cleared)
+        self.current_piece = self.next_piece
+        self.next_piece = Piece()
+        if not self.grid.is_valid_position(self.current_piece):
+            self.game_over = True
+
+    def reset(self):
+        self.grid.reset()
+        self.score_manager.reset()
+        self.current_piece = Piece()
+        self.next_piece = Piece()
+        self.fall_timer = 0
+        self.game_over = False
+
+
+''' #TODO
+update, run прописати. 
+'''
