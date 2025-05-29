9f6da50d0d042ff8f1488b853d55d8fb4f090a05
Maksym Mokrytskyi <maks.mokrytskyi.1234@gmail.com>
2025-05-29 15:59:54 +0300

    Completed preview

--- TetrisRender.py
+++ TetrisRender.py
@@ -33,3 +33,23 @@ class TetrisRenderer:
             s = pygame.Surface((CELL, CELL), pygame.SRCALPHA)
             s.fill((*self.colors[color][:3], 40))
             screen.blit(s, rect.topleft)
+
+    def _draw_next_piece(self, screen, piece):
+        preview_x = WIDTH + 20
+        preview_y = 107
+        font = pygame.font.SysFont('consolas', 20)
+        text = font.render('NEXT:', True, (255, 255, 255))
+        screen.blit(text, (preview_x, preview_y - 23))
+        pygame.draw.rect(screen, (40, 40, 40), (preview_x - 5, preview_y - 5, 110, 110))
+        pygame.draw.rect(screen, (100, 100, 100), (preview_x - 5, preview_y - 5, 110, 110), 2)
+        offset_x = (4 - piece.shape.shape[1]) * CELL // 4
+        offset_y = (4 - piece.shape.shape[0]) * CELL // 4
+        for y in range(piece.shape.shape[0]):
+            for x in range(piece.shape.shape[1]):
+                if piece.shape[y, x]:
+                    small_rect = pygame.Rect(
+                        preview_x + x * 20 + offset_x,
+                        preview_y + y * 20 + offset_y,
+                        20, 20
+                    )
+                    self._draw_block(screen, self.colors[piece.color], small_rect)
